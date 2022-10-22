dee0e5f5764647e9cb054ab74c4b285f73a460bb
Maulana Muhammad Rizky <maulana9175@gmail.com>
2022-10-22 11:03:22 +0700

    UPDATE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        .button-custom {
+            padding: 10;
+            background-color: green;
+            color: whitesmoke;
+            border: 10;
+            box-shadow: 0cm;
+            width: 100px;
+            height: 100px;
+        }
+    </style>
 </head>
 <body>
     <marquee behavior="100" direction="">
         <h3>BELAJAR LARAVEL</h3>
     </marquee>
     <h1>INI UDAH UPDATE LOH </h1>
+    <image src="link"/>
+    <button class="button-custom" onclick="Alert()">
+        INI HOME
+    </button>
+
+    {{-- JAVASCRIPT --}}
+    <script>
+        const Alert = () => {
+            alert("AwWW")
+        }
+    </script>
 </body>
 </html>
